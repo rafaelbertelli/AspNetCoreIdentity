3ed6f19072de3aa13cdef9024ee969c895a0f31a
Rafael Borges <rafaelbertelli89@gmail.com>
2020-01-10 21:17:12 -0300

    Add project files.

---
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "PodeExcluir")]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "PodeExcluir")]
+        public IActionResult SecretClaim()
+        {
+            return View("Index");
+        }
+
+        [Authorize(Policy = "PodeGravar")]
+        public IActionResult SecretClaimGravar()
+        {
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
